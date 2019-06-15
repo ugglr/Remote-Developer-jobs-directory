dcf10987b333de107b107ecd5af9ed1f885dc3d8
Ugglr <carl.igelstrom@gmail.com>
2019-06-15 16:29:57 +0800

    Headelines

--- src/pages/codingTests.js
+++ src/pages/codingTests.js
@@ -10,7 +10,8 @@ class CodingTests extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <h1>Hello from coding tests page.</h1>
+        <h1>Front-End Tests</h1>
+        <h1>Back-End Tests</h1>
       </Layout>
     )
   }
