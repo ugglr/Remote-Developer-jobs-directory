53968301b294e66a0b9fd02247203121bcf9be2a
Ugglr <carl.igelstrom@gmail.com>
2022-09-08 21:47:19 +0800

    add google analytics

---
+++ lib/gtag.ts
@@ -0,0 +1,27 @@
+export const GA_TRACKING_ID = process.env.GA_TRACKING_ID || "";
+
+// Next.js example
+//https://github.com/vercel/next.js/tree/canary/examples/with-google-analytics
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: URL): void => {
+  window.gtag("config", GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
+type GTagEvent = {
+  action: string;
+  category: string;
+  label: string;
+  value: number;
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }: GTagEvent): void => {
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value,
+  });
+};
