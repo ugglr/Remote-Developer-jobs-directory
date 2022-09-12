9f04cc53e3cd6b23653803826e819960042643c6
Ugglr <carl.igelstrom@gmail.com>
2022-09-12 13:56:36 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Card from "../components/Card";
 import { freelancePlatforms, jobBoards, slackChannels } from "../content";
 
-import styles from "../../styles/Home.module.scss";
+import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
