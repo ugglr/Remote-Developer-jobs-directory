1835c508fbb08b484eda9200e642a81b81deb0bd
Ugglr <carl.igelstrom@gmail.com>
2022-10-30 23:17:57 +0800

    Page added.

---
+++ pages/checklists/remote-ready/index.tsx
@@ -0,0 +1,23 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const RemoteReadyChecklist: NextPage = () => (
+  <div>
+    <Head>
+      <title>
+        Remote junior developer jobs director | Remote Ready Checklist
+      </title>
+      <meta
+        name="description"
+        content="Check the list if you are remote ready"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      <h1>Remote Ready checklist</h1>
+    </main>
+  </div>
+);
+
+export default RemoteReadyChecklist;
