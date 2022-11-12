915ab09177a9f023dd51a95c97de7af001b00933
Ugglr <carl.igelstrom@gmail.com>
2022-11-12 23:57:23 +0800

    cleanup again

--- content/index.ts
+++ content/index.ts
@@ -1,4 +1,3 @@
-export { default as companies } from "./companies";
 export { default as freelancePlatforms } from "./freelancePlatforms";
 export { default as jobBoards } from "./jobBoards";
 export { default as communities } from "./communities";
