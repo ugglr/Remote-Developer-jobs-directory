0462bcebde409db968fce3bef60fef61b10e7736
Ugglr <carl.igelstrom@gmail.com>
2022-11-13 23:23:21 +0800

    transfer complete.

---
+++ content/communities/dev-chat.md
@@ -0,0 +1,5 @@
+---
+name: DevChat
+description: Join the jobs channel and keep a look-out. Also a place where it's possible to ask for help when stuck.
+url: https://devchat.webflow.io
+---
--- content/index.ts
+++ content/index.ts
@@ -1,3 +1,2 @@
 export { default as freelancePlatforms } from "./freelancePlatforms";
 export { default as jobBoards } from "./jobBoards";
-export { default as communities } from "./communities";
