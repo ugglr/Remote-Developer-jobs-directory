743be9898ae0d8a6b20cec1e913a70a2cf5f5ea2
Ugglr <carl.igelstrom@gmail.com>
2022-11-24 11:34:18 +0800

    migration complete.

--- utils.ts
+++
@@ -1,5 +0,0 @@
-// Checks if x lies between min and max
-// return true if in range, otherwise false
-export const inRange = (x: number, min: number, max: number) => {
-  return (x - min) * (x - max) <= 0;
-};
